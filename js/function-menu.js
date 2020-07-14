444a9df246831a9f6464818f739464f9ac4d2881
Yuri-sc <yurisantiagodacosta@gmail.com>
2020-07-14 16:32:17 -0300

    commit finalização dos comandos iniciais.

---
+++ js/function-menu.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+    // Eu quero abrir a pagina list-disciplinas.html dentro do index.html
+
+    $('#conteudo').load('src/disciplinas/visao/list-disciplinas.html')
+})
